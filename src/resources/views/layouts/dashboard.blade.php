88bffb77be2dc1bfc96abba54d9bc68595a2b800
unknown <shofiecilia2229@gmail.com>
2025-09-08 16:07:32 +0700

    shofie home1

---
+++ src/resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>
+      <title>{{ config('app.name', 'Laravel') }} - @yield('title')</title> 
+      @vite(['resources/css/app.css', 'resources/js/app.js'])
+      <!-- gpt -->
+    </title>
+  </head>
+  <body
+    x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': false, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
+    x-init="
+         darkMode = JSON.parse(localStorage.getItem('darkMode'));
+         $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+    :class="{'dark bg-gray-900': darkMode === true}"
+  >
+    <!-- ===== Preloader Start ===== -->
+     @include('partials.preloader')
+    <!-- ===== Preloader End ===== -->
+
+    <!-- ===== Page Wrapper Start ===== -->
+    <div class="flex h-screen overflow-hidden">
+      <!-- ===== Sidebar Start ===== -->
+       @include('partials.sidebar')
+      <!-- ===== Sidebar End ===== -->
+
+      <!-- ===== Content Area Start ===== -->
+      <div
+        class="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto"
+      >
+        <!-- Small Device Overlay Start -->
+         @include('partials.overlay')
+        <!-- Small Device Overlay End -->
+
+        <!-- ===== Header Start ===== -->
+         @include('partials.header')
+        <!-- ===== Header End ===== -->
+
+        <!-- ===== Main Content Start ===== -->
+        <main>
+          <div class="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
+            <div class="grid grid-cols-12 gap-4 md:gap-6">
+              <div class="col-span-12 space-y-6 xl:col-span-7">
+                <!-- Metric Group One -->
+                @include('partials.metric-group.metric-group-01')
+                <!-- Metric Group One -->
+
+                <!-- ====== Chart One Start -->
+                @include('partials.chart.chart-01')
+                <!-- ====== Chart One End -->
+              </div>
+            </div>
+          </div>
+        </main>
+        <!-- ===== Main Content End ===== -->
+      </div>
+      <!-- ===== Content Area End ===== -->
+    </div>
+    <!-- ===== Page Wrapper End ===== -->
+  </body>
+</html>
