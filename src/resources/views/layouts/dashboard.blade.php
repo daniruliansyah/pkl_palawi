d30cae03bdcb376e17937652058cdf9b61a4f2f9
unknown <shofiecilia2229@gmail.com>
2025-09-09 13:22:42 +0700

    gausa di merge

--- src/resources/views/layouts/dashboard.blade.php
+++ src/resources/views/layouts/dashboard.blade.php
@@ -1,17 +1,13 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-    />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>
-      <title>{{ config('app.name', 'Laravel') }} - @yield('title')</title> 
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+      <title>{{ config('app.name', 'Laravel') }} - @yield('title', 'Dashboard')</title>
+
       @vite(['resources/css/app.css', 'resources/js/app.js'])
-      <!-- gpt -->
-    </title>
   </head>
   <body
     x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': false, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
